8300ff835ec61bd1ea938d1bfe8f694979539225
m x <zerogamepe@gmail.com>
2022-06-08 18:22:42 +0300

    kkk

--- scripts/mint-script.js
+++ scripts/mint-script.js
@@ -4,10 +4,10 @@ var ethers = require('ethers');
 const BigNumber = require("bignumber.js");
 async function main() {
   const NFT = await hre.ethers.getContractFactory("PRODUCT_NFT");
-  const URI = "https://ipfs.io/ipfs/bafkreiey6pam74dav2km3gnrkqtkfdtkpcmeilyiqeohnrf6v2d4yklr5u"
+  const URI = "ipfs://bafyreiffb5oauvg5frdl4j2yxr66sqb326sv5qmlcqvj4nnmr7oqa7vtyi/metadata.json"
   const WALLET_ADDRESS = "0x1dA45683bd3ccd6f8308050d0D99c1ee7F761E5f"
   const CONTRACT_ADDRESS = "0xD3a41C60752f4F359a19f36770fB5F91108bb666"
-  token_id =ethers.BigNumber.from("0x1dA45683bd3ccd6f8308050d0D99c1ee7F761E5f")
+  token_id =ethers.BigNumber.from("0x3e109fac936347c72e944caf923df27f811a202f52f24c2c94529c390c99b511")
   const contract = NFT.attach(CONTRACT_ADDRESS);
   await contract.mint(WALLET_ADDRESS, token_id,URI);
   console.log("NFT minted:", contract);
